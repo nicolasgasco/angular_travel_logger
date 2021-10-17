893b88579609708cbfd2b1a356bfeba31b4c1f28
nicolasgasco <nicolas.gasco92@gmail.com>
2021-10-17 19:44:05 +0200

    Added login icon

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -3,18 +3,21 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 @NgModule({
   imports: [
     MatButtonModule,
     MatFormFieldModule,
     MatInputModule,
     MatCardModule,
+    MatIconModule,
   ],
   exports: [
     MatButtonModule,
     MatFormFieldModule,
     MatInputModule,
     MatCardModule,
+    MatIconModule,
   ],
 })
 export class MaterialModule {}
