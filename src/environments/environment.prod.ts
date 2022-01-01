cac548b3316e05f93ade441408abbe182957a997
Nicolas Gasco <nicolas.gasco92@gmail.com>
2022-01-01 20:20:10 +0100

    Preparing for deployment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAE3ttH2a_N_dbqv3Z63Luk2QzBh-6bNAI',
+    authDomain: 'ng-travel-logger.firebaseapp.com',
+    projectId: 'ng-travel-logger',
+    storageBucket: 'ng-travel-logger.appspot.com',
+    messagingSenderId: '442477044140',
+    appId: '1:442477044140:web:94ee6c0b5f1efc77b75a09',
+    measurementId: 'G-X0VVV43XWR',
+  },
 };
