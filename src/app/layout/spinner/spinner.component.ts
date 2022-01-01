42f67863bd9f3d3fec8821f561b412d1ccac06c2
Nicolas Gasco <nicolas.gasco92@gmail.com>
2022-01-01 11:32:53 +0100

    Added generic spinner

---
+++ src/app/layout/spinner/spinner.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'travel-log-spinner',
+  template: `Spinner works!`,
+  styleUrls: ['./spinner.component.scss'],
+})
+export class SpinnerComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
