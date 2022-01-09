da5d25e0341d4c2282fab5d432d8b37ea6c2cfd6
Nicolas Gasco <nicolas.gasco92@gmail.com>
2022-01-09 18:29:28 +0100

    Removed leftover code

--- src/app/trips/trip-card/journal-expansion/journal-expansion.component.ts
+++ src/app/trips/trip-card/journal-expansion/journal-expansion.component.ts
@@ -2,37 +2,36 @@ import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'travel-log-journal-expansion',
-  template: ` {{ entries | json}}
-    <mat-expansion-panel
-      (opened)="panelOpenState = true"
-      (closed)="panelOpenState = false"
-    >
-      <mat-expansion-panel-header>
-        <mat-panel-title>Journal</mat-panel-title>
-        <mat-panel-description>
-          {{ entries.length }} {{ entries.length === 1 ? 'entry' : 'entries' }}
-        </mat-panel-description>
-      </mat-expansion-panel-header>
-      <ul role="list">
-        <ng-container *ngFor="let journalEntry of entries; let i = index">
-          <li role="listitem">
-            <p>
-              <span class="journal-date-label">
-                {{
-                  journalEntry.day.toLocaleDateString('en-US', {
-                    day: 'numeric',
-                    month: 'long'
-                  })
-                }}</span
-              >: {{ journalEntry.entry }}
-            </p>
-            <!-- <mat-divider
+  template: ` <mat-expansion-panel
+    (opened)="panelOpenState = true"
+    (closed)="panelOpenState = false"
+  >
+    <mat-expansion-panel-header>
+      <mat-panel-title>Journal</mat-panel-title>
+      <mat-panel-description>
+        {{ entries.length }} {{ entries.length === 1 ? 'entry' : 'entries' }}
+      </mat-panel-description>
+    </mat-expansion-panel-header>
+    <ul role="list">
+      <ng-container *ngFor="let journalEntry of entries; let i = index">
+        <li role="listitem">
+          <p>
+            <span class="journal-date-label">
+              {{
+                journalEntry.day.toLocaleDateString('en-US', {
+                  day: 'numeric',
+                  month: 'long'
+                })
+              }}</span
+            >: {{ journalEntry.entry }}
+          </p>
+          <!-- <mat-divider
             *ngIf="entries.length > 1 && i !== entries.length - 1"
           ></mat-divider> -->
-          </li>
-        </ng-container>
-      </ul>
-    </mat-expansion-panel>`,
+        </li>
+      </ng-container>
+    </ul>
+  </mat-expansion-panel>`,
   styleUrls: ['./journal-expansion.component.scss'],
 })
 export class JournalExpansionComponent implements OnInit {
