d5005f596a6f7d118d3c271200206cdc2f1ed64c
Nicolas Gasco <nicolas.gasco92@gmail.com>
2022-01-12 17:14:18 +0100

    Fixed issue with footer on MacOs

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -5,7 +5,7 @@ import { Component, OnInit } from '@angular/core';
   template: `<footer>
     <div class="max-container">
       <small
-        >Made with 🤍 by
+        >Made with ♡ by
         <a
           href="https://github.com/nicolasgasco"
           target="_blank"
